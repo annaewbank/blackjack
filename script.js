437ea73bbb1509abfc28242571f42dc04a5047a7
Anna Ewbank <92327363+annaewbank@users.noreply.github.com>
2023-11-19 11:40:33 +0000

    Add files via upload
    Updated with jQuery

--- script.js
+++ script.js
@@ -27,29 +27,30 @@ const values = [
   "K",
 ];
 const suits = ["H", "D", "S", "C"];
-const hitButton = document.getElementById("hit");
-const standButton = document.getElementById("stand");
-const replayButton = document.getElementById("new-game");
+const hitButton = $("#hit");
+const standButton = $("#stand");
+const replayButton = $("#new-game");
 let playerCanHit = true; // Allows the player to draw while playerRoundScore < 21
 let standButtonClicked = false; // Replay button can be clicked when standButtonClicked = true
+let dealingInProgress = false;
 
 // Player variables
-const playerCards = document.getElementById("your-cards");
+const playerCards = $("#player-cards");
 let playerAceCount = 0; // Tracks how many aces the player has
 let playerRoundScore;
 
 // Dealer variables
-const dealerCards = document.getElementById("dealer-cards");
+const dealerCards = $("#dealer-cards");
 let dealerHiddenCard; // Tracks the dealer's hidden card
-let dealerHiddenCardImg = document.getElementById("hidden");
+let dealerHiddenCardImg = $("#hidden");
 let dealerAceCount = 0; // Tracks how many aces the dealer has
 let dealerRoundScore;
 
 // Modal variables
-const rulesButton = document.getElementById("rules-modal");
-const rulesModal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const closeModalButton = document.querySelector(".close-modal");
+const rulesButton = $("#rules-modal");
+const rulesModal = $(".modal");
+const overlay = $(".overlay");
+const closeModalButton = $(".close-modal");
 
 // FUNCTIONS
 // Function to build the deck
@@ -128,23 +129,23 @@ const reduceDealerAce = function (roundScore, aceCount) {
 
 // Function to update the current score text content
 const updateRoundScore = function (whichPlayer) {
-  document.getElementById("round-score-" + whichPlayer).textContent =
-    whichPlayer === "dealer" ? dealerRoundScore : playerRoundScore;
+  $("#round-score-" + whichPlayer).text(
+    whichPlayer === "dealer" ? dealerRoundScore : playerRoundScore
+  );
 };
 
 // Function to deal a card
 const dealACard = function (whichPlayer) {
-  let cardImg = document.createElement("img"); // Creates an img tag <img>
-  let newCard = deck.pop(); // Assigns newCard variable a value from the deck
-  cardImg.src = "./cards/" + newCard + ".png"; // Assigns a source to the img tag
-  document.getElementById(whichPlayer + "-cards").append(cardImg); // Appends new img tag to either the player-cards or dealer-cards element
+  let newCard = deck.pop();
 
   if (whichPlayer === "dealer") {
+    dealerCards.append($("<img>").attr("src", "./cards/" + newCard + ".png"));
     dealerRoundScore += getCardValue(newCard);
     dealerAceCount += checkIfAce(newCard);
     dealerRoundScore = reduceDealerAce(dealerRoundScore, dealerAceCount);
     updateRoundScore("dealer");
-  } else {
+  } else if (whichPlayer === "player") {
+    playerCards.append($("<img>").attr("src", "./cards/" + newCard + ".png"));
     playerRoundScore += getCardValue(newCard);
     playerAceCount += checkIfAce(newCard);
     playerRoundScore = reducePlayerAce(playerRoundScore, playerAceCount);
@@ -152,12 +153,11 @@ const dealACard = function (whichPlayer) {
 
     // Stop round if player goes bust
     if (playerRoundScore > 21) {
-      document.getElementById("results").innerText = "Bust!\nDealer wins.";
-      document.body.classList.remove("normal-background");
-      document.body.classList.add("lose-background");
+      $("#results").text("Bust!\nDealer wins.");
+      $("body").removeClass("normal-background").addClass("lose-background");
       playerCanHit = false; // Disable the hit button
       standButtonClicked = true; // Disable the stand button
-      replayButton.addEventListener("click", startGame); // Enable the replay button
+      replayButton.on("click", startGame); // Enable the replay button
     }
   }
 };
@@ -165,8 +165,9 @@ const dealACard = function (whichPlayer) {
 // Function for when the hit button is clicked
 const hit = function () {
   // Disable the hit button if score >= 21
-  if (playerRoundScore >= 21) {
+  if (playerRoundScore >= 21 || dealingInProgress) {
     playerCanHit = false;
+    return;
   }
 
   // Exit the function if playerCanHit is falsy
@@ -174,7 +175,19 @@ const hit = function () {
     return;
   }
 
+  dealingInProgress = true; // Set the flag to indicate dealing is in progress
   dealACard("player"); // Else, deal a card
+
+  // Enable the hit button after a short delay (adjust the delay as needed)
+  setTimeout(() => {
+    dealingInProgress = false;
+    playerCanHit = true;
+  }, 500);
+  /*
+  dealingInProgress and setTimeout()
+  Credit: ChatGPT
+  I originally made my game without jQuery and everything worked. However, when adding jQuery, occasionally when pressing 'Hit', the player would be dealt more that one card. I'm not sure why this started to happen - the 'Hit' button was only clicked once, but adding a dealingInProgress flag and a setTimeout() function to disable and enable the hit button seems to have fixed it.
+*/
 };
 
 // Function for when the stand button is clicked
@@ -187,8 +200,7 @@ const stand = function () {
   playerCanHit = false; // Disable the hit button
 
   // Reveal the hidden card
-  document.getElementById("hidden").src =
-    "./cards/" + dealerHiddenCard + ".png";
+  $("#hidden").attr("src", "./cards/" + dealerHiddenCard + ".png");
   // Update dealer's score
   dealerRoundScore += getCardValue(dealerHiddenCard);
   // Reduce score if dealer is dealt 2 aces
@@ -210,25 +222,24 @@ const stand = function () {
   let message = "";
   if (dealerRoundScore > 21) {
     message = "Dealer is bust.\nYou win!";
-    document.body.classList.remove("normal-background");
-    document.body.classList.add("win-background");
+    $("body").removeClass("normal-background").addClass("win-background");
   } else if (playerRoundScore === dealerRoundScore) {
     message = "It's a tie!";
   } else if (playerRoundScore > dealerRoundScore) {
     message = "You win!";
-    document.body.classList.remove("normal-background");
-    document.body.classList.add("win-background");
+    $("body").removeClass("normal-background").addClass("win-background");
   } else if (playerRoundScore < dealerRoundScore) {
     message = "You lose!";
-    document.body.classList.remove("normal-background");
-    document.body.classList.add("lose-background");
+    $("body").removeClass("normal-background").addClass("lose-background");
   }
-  document.getElementById("results").innerText = message;
+  $("#results").text(message);
 
-  replayButton.addEventListener("click", startGame); // Enable replay button
+  replayButton.on("click", startGame); // Enable replay button
 
   standButtonClicked = true; // Update standButtonClicked flag so it can't be clicked again
 };
+
+// ---
 
 // Function to start/reset the game
 function startGame() {
@@ -239,8 +250,9 @@ function startGame() {
   shuffleDeck();
 
   // Reset background
-  document.body.classList.add("normal-background");
-  document.body.classList.remove("win-background", "lose-background");
+  $("body")
+    .addClass("normal-background")
+    .removeClass("win-background lose-background");
 
   // Reset round scores and ace counts to 0
   dealerRoundScore = 0;
@@ -253,20 +265,20 @@ function startGame() {
   standButtonClicked = false;
 
   // Reset results message
-  document.getElementById("results").innerHTML = "";
-  document.getElementById("player-cards").innerHTML = "";
-  document.getElementById("dealer-cards").innerHTML = "";
+  $("#results").html("");
+  playerCards.html("");
+  dealerCards.html("");
 
   // Remove Replay button event listener
-  replayButton.removeEventListener("click", startGame);
+  replayButton.off("click", startGame);
 
   // Deal 2 cards to the dealer
-  let cardImg = document.createElement("img"); // Creates an img tag <img>
+  let cardImg = $("<img>"); // Creates an img tag <img>
   dealerHiddenCard = deck.pop(); // Assigns the dealerHiddenCard variable a value from the deck
   dealerAceCount += checkIfAce(dealerHiddenCard); // Updates the dealer's ace count
-  cardImg.src = "./cards/BACK.png"; // Assigns a source to the img tag
-  cardImg.id = "hidden"; // Assigns an ID to the img tag
-  document.getElementById("dealer-cards").append(cardImg); // Appends new img tag to the dealer-cards element
+  cardImg.attr("src", "./cards/BACK.png"); // Assigns a source to the img tag
+  cardImg.attr("id", "hidden"); // Assigns an ID to the img tag
+  dealerCards.append(cardImg); // Appends new img tag to the dealer-cards element
   dealACard("dealer"); // Deal a visible card to the dealer
   updateRoundScore("dealer");
 
@@ -276,8 +288,8 @@ function startGame() {
   }
 
   // Make the hit and stand buttons functional
-  hitButton.addEventListener("click", hit);
-  standButton.addEventListener("click", stand);
+  hitButton.on("click", hit);
+  standButton.on("click", stand);
 }
 
 startGame();
@@ -285,24 +297,24 @@ startGame();
 // MODAL FUNCTIONS
 // Create a function to open the modal
 const openModal = function () {
-  rulesModal.classList.remove("hidden");
+  rulesModal.removeClass("hidden");
   // classList returns a collection of the element's classes
   // Note that the dot selector is not used
-  overlay.classList.remove("hidden");
+  overlay.removeClass("hidden");
 };
 
 // Create a function to close the modal
 const closeModal = function () {
-  rulesModal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  rulesModal.addClass("hidden");
+  overlay.addClass("hidden");
 };
 
 // MODAL EVENT LISTENERS
 // Make the rules button functional
-rulesButton.addEventListener("click", openModal);
+rulesButton.on("click", openModal);
 
 // Close the modal by clicking the cross button
-closeModalButton.addEventListener("click", closeModal);
+closeModalButton.on("click", closeModal);
 
 // Close the modal by clicking on the overlay
-overlay.addEventListener("click", closeModal);
+overlay.on("click", closeModal);
